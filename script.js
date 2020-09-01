e743e7be0ecba7705469cbd1d00a80560ee2ffb2
Camty1 <cwolfe@vms.edu>
2020-08-31 20:29:23 -0600

    Animation testing
    CSS animations will not be sufficient for this project

---
+++ script.js
@@ -0,0 +1,10 @@
+playFlip(elem) {
+  elem.style.animation-name: "hoverFlip";
+  elem.style.animation-duration: "1s";
+
+}
+
+reverseFlip(elem) {
+  elem.style.animation-name: "reverseHoverFlip";
+  elem.style.animaiton-duration: "1s";
+}
